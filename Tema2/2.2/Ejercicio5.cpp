c7ddb9cd4b3bb9b3ff33d79ca79d25d83aa21557
Mercedes Roman Ruiz <122405877+mercedesromanruiz@users.noreply.github.com>
2023-01-13 11:30:13 -0600

    Create Ejercicio5.cpp

---
+++ Tema2/2.2/Ejercicio5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const double Constante_de_Planck = 6.626e-34;
+	const double Masa_de_la_Tierra = 5.9722e24;
+	const double Masa_del_Sol = 1.9891e30;
+
+	cout << "Constante de Planck: " << Constante_de_Planck << endl;
+	cout << "Masa de la Tierra: " << Masa_de_la_Tierra << endl;
+	cout << "Masa del Sol: " << Masa_del_Sol << endl;
+}
