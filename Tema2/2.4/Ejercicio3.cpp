6f3bdf05b19ec2574acbf3f7921d434fa7cc816f
Mercedes Roman Ruiz <122405877+mercedesromanruiz@users.noreply.github.com>
2023-01-26 11:16:33 -0600

    Create Ejercicio3.cpp

---
+++ Tema2/2.4/Ejercicio3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "Introduce 5 número a sumar: " << endl;
+	for (int i = 0; i++; i < 5) {
+		cin >> a;
+		b = b + a;
+	}
+	cout << "Resultado: " << b << endl;
+}
